9fda7d182700b6c7b019d1265a6d80a257378ae8
Kieran Llarena <baymax709@gmail.com>
2023-11-03 13:53:46 -0400

    added prop types, created mobile design

--- src/components/SpeakerSessionCard.jsx
+++ src/components/SpeakerSessionCard.jsx
@@ -1,56 +1,91 @@
-import { ReactPropTypes } from "react"
+import {  useState } from "react"
+import PropTypes from 'prop-types'
 
-function SpeakerSessionCard({ name, sessionTitle, speakerImg }) {
+/*
+
+[Card Usage Tip]
+
+Desc prop must be passed using `` in order for the <pre> tag to work
+
+Ex. this formatting gives the same as the designs
+
+`We all know that providing a good search experience for website visitors is hard. Everyone expects it to “just” work like Bing or Amazon. Maybe there is a better way, maybe we fix this with a little help from some AI or even our new friend Chat GPT.
+          
+Join the session to see how you can leverage Cognitive Services with Azure Cognitive Search in your apps. With the newly minted semantic search and OpenAI abilities this technology can provide results that are semantically closer to the intent of original query.
+
+You can expect to walk away knowing how to generate your own power search experience that leverages the power of AI to solve your search woes.`
+
+*/
+
+function SpeakerSessionCard({ name, title, speakerImg, desc }) {
+  const [toggled, toggleDropdown] = useState(false)
+
   return (
     <section
-      className="flex w-5/6 items-center justify-between rounded-xl border py-2.5 pl-12 pr-24 shadow-lg transition duration-200 hover:shadow-xl"
+      className="w-5/6 rounded-xl border p-2.5 shadow-lg transition duration-200 hover:shadow-xl"
     >
       <section
-        className="flex items-center"
+        className="flex items-center justify-between"
       >
-        <img
-          src={speakerImg}
-          alt=""
-          className="h-40 rounded-full"
-        />
-        <div
-          className="pl-12 "
+        <section
+          className="flex items-center"
         >
-          <h3
-            className="mb-6 text-3xl font-bold"
-          >
-            {sessionTitle}
-          </h3>
-          <p
-            className="text-gray-700"
-          >
-            by {name}
-          </p>
-        </div>
-      </section>
-      <button>  
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none" viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="h-20 w-20">
-          
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          <img
+            src={speakerImg}
+            alt={`Headshot of ${name}`}
+            className="hidden h-40 rounded-full md:block"
           />
-        </svg>
-      </button>
+          <div>
+            <h3
+              className="font-bold"
+            >
+              {title}
+            </h3>
+            <p
+              className="text-gray-700"
+            >
+              by {name}
+            </p>
+          </div>
+        </section>
+        <button
+          onClick={() => toggleDropdown(!toggled)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none" viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={`h-10 w-10 transition md:h-20 md:w-20 ${toggled ? "rotate-180" : "rotate-0"} duration-300`}>
+            
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+      </section>
+      {toggled ? (
+        <section
+          className="mt-5 w-10/12 pl-10"
+        >
+          <pre
+            className="whitespace-pre-wrap border-t border-gray-700 py-5"
+          >
+            {desc}
+          </pre>
+        </section>
+      ): ''}
     </section>
   )
 }
 
 SpeakerSessionCard.propTypes = {
-  name: ReactPropTypes.string,
-  sessionTitle: ReactPropTypes.string,
-  speakerImg: ReactPropTypes.string
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  speakerImg: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired
 }
 
 export default SpeakerSessionCard
