1b09d9bfb391fe0fe69afc72ec41b16c74db569b
Kieran Llarena <baymax709@gmail.com>
2023-11-05 14:07:35 -0500

    toggled => isToggled, better prop names

--- src/components/SpeakerSessionCard.jsx
+++ src/components/SpeakerSessionCard.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react"
 import PropTypes from 'prop-types'
 
-function SpeakerSessionCard({ name, title, speakerImg, desc }) {
-  const [toggled, toggleDropdown] = useState(false)
+// fix padding responsiveness
+function SpeakerSessionCard({ speakerName, sessionTitle, speakerImg, sessionDesc }) {
+  const [ isToggled, setIsToggled ] = useState(false)
 
   return (
     <section
       className="rounded-xl border bg-white shadow-lg transition duration-200 hover:shadow-xl"
     >
       <button
-        onClick={() => toggleDropdown(!toggled)}
-        className="flex w-full items-center justify-between p-2.5"
+        onClick={() => setIsToggled(!isToggled)}
+        className="flex w-full items-center justify-between p-2.5 pl-5 sm:px-10 md:pl-10 md:pr-14 xl:pr-24"
       >
         <section
           className="flex items-center text-left"
         >
           <img
             src={speakerImg}
-            alt={`Headshot of ${name}`}
+            alt={`Headshot of ${speakerName}`}
             className="hidden h-40 rounded-full md:block"
           />
           <div
@@ -26,12 +27,12 @@ function SpeakerSessionCard({ name, title, speakerImg, desc }) {
             <h3
               className="font-bold md:mb-2.5 md:text-xl lg:text-2xl xl:mb-5 xl:text-3xl"
             >
-              {title}
+              {sessionTitle}
             </h3>
             <p
               className="text-gray-700"
             >
-              by {name}
+              by {speakerName}
             </p>
           </div>
         </section>
@@ -40,7 +41,7 @@ function SpeakerSessionCard({ name, title, speakerImg, desc }) {
           fill="none" viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className={`h-10 w-10 transition md:h-12 md:w-12 xl:h-20 xl:w-20 ${toggled ? "rotate-180" : "rotate-0"} duration-300`}>
+          className={`h-10 w-10 transition md:h-12 md:w-12 xl:h-20 xl:w-20 ${isToggled ? "rotate-180" : "rotate-0"} duration-300`}>
           
           <path
             strokeLinecap="round"
@@ -49,14 +50,14 @@ function SpeakerSessionCard({ name, title, speakerImg, desc }) {
           />
         </svg>
       </button>
-      {toggled ? (
+      {isToggled ? (
         <section
           className="mt-5 w-10/12 pl-12"
         >
           <p
             className="whitespace-pre-wrap border-t border-gray-700 pb-10 pt-5"
           >
-            {desc}
+            {sessionDesc}
           </p>
         </section>
       ): null}
@@ -65,10 +66,10 @@ function SpeakerSessionCard({ name, title, speakerImg, desc }) {
 }
 
 SpeakerSessionCard.propTypes = {
-  name: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
+  speakerName: PropTypes.string.isRequired,
+  sessionTitle: PropTypes.string.isRequired,
   speakerImg: PropTypes.string.isRequired,
-  desc: PropTypes.string.isRequired
+  sessionDesc: PropTypes.string.isRequired
 }
 
 export default SpeakerSessionCard
