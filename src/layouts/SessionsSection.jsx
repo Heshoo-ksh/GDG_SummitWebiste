759ede3243d9376fd195764db9e0fe7ce13581ba
Kieran Llarena <baymax709@gmail.com>
2023-11-05 20:32:53 -0500

    turned sessions into js file

--- src/layouts/SessionsSection.jsx
+++ src/layouts/SessionsSection.jsx
@@ -1,13 +1,8 @@
 import SessionList from "@/components/SessionList"
+import { SpeakerSessions } from "@/data/sessions"
 
 function SessionsSection() {
   // add actual sessions, these are here as placeholder
-  const sessions = [
-    { speakerName: "Madona Wambua", sessionTitle: "Lorem Ipsum Title of the Workshop", speakerImg: "https://ui-avatars.com/api/?name=test&background=random", sessionDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-    { speakerName: "Erica Olson", sessionTitle: "Lorem Ipsum Title of the Workshop", speakerImg: "https://ui-avatars.com/api/?name=test&background=random", sessionDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." },
-    { speakerName: "Brian McKeiver", sessionTitle: "State-of-the-art Search with Azure AI Cognitive Search", speakerImg: "https://ui-avatars.com/api/?name=test&background=random", sessionDesc: "We all know that providing a good search experience for website visitors is hard. Everyone expects it to “just” work like Bing or Amazon. Maybe there is a better way, maybe we fix this with a little help from some AI or even our new friend Chat GPT.\n\nJoin the session to see how you can leverage Cognitive Services with Azure Cognitive Search in your apps. With the newly minted semantic search and OpenAI abilities this technology can provide results that are semantically closer to the intent of original query.\n\nYou can expect to walk away knowing how to generate your own power search experience that leverages the power of AI to solve your search woes." }
-  ]
-
   return (
     <section
       id="sessions"
@@ -21,7 +16,7 @@ function SessionsSection() {
             on what is selected in the sessions categories tab using child callback
         */
       }
-      <SessionList session={sessions} />
+      <SessionList session={SpeakerSessions} />
     </section>
   )
 }
