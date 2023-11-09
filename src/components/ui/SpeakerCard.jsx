bad89892361f3a1b726144885f52d21666156750
Hisham Odeh <hishamodeh2001@gmail.com>
2023-11-09 14:50:01 -0500

    Fix padding and round space

--- src/components/ui/SpeakerCard.jsx
+++ src/components/ui/SpeakerCard.jsx
@@ -5,16 +5,16 @@ const SpeakerCard = ({ name, twitterHandle, imageUrl, companyName }) => {
   return (
     <div className="relative h-[246.52px] w-[329.30px] rounded-[3px] bg-blue-500 shadow">
       {/* Image circle, positioned to overlap the top of the rectangle */}
-      <div className="flex justify-center" style={{ marginTop: '-137.5px' }}>
+      <div className="mt-[-137.5px] flex justify-center">
         <div className="relative">
           <img
-            className="h-[275px] w-[273px] rounded-full border-4 border-white object-cover"
+            className="h-[275px] w-[273px] rounded-full object-cover"
             src={imageUrl}
             alt={name}
           />
           {/* Twitter Icon - Conditionally rendered */}
           {twitterHandle && (
-  <div className="absolute bottom-[-27px] left-1/2 flex h-[60px] w-[60px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-white">
+  <div className="absolute bottom-[-31px] left-1/2 flex h-[60px] w-[60px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-white">
     <img className="h-10 w-10" src={twitterLogo} alt="Twitter logo" />
   </div>
 )}
@@ -23,7 +23,7 @@ const SpeakerCard = ({ name, twitterHandle, imageUrl, companyName }) => {
       </div>
 
       {/* Rectangle for name and company */}
-      <div className="absolute bottom-0 w-full px-4 pb-4 pt-[60px]">
+      <div className="absolute bottom-0 w-full px-4 pb-10 ">
         <div className="text-center font-['Open_Sans'] text-2xl font-normal text-white">{name}</div>
         <div className="mt-2 text-center font-['Open_Sans'] text-sm font-normal text-white">{companyName}</div>
       </div>
