e352d2851b700ee9b32cf2057aaabf1693c7cacc
Kieran Llarena <baymax709@gmail.com>
2023-11-12 14:49:29 -0500

    fixed ui avatar issues

--- src/components/SpeakerSessionCard.jsx
+++ src/components/SpeakerSessionCard.jsx
@@ -26,13 +26,21 @@ function SpeakerSessionCard({
         className="flex w-full items-center justify-between p-2.5 pl-5 sm:px-10 md:pl-10 md:pr-14 xl:pr-24"
       >
         <section className="flex items-center text-left">
-          <img
-            src={speakerAvatar}
-            alt={`Headshot of ${speakerName}`}
-            className="hidden h-40 rounded-full md:block"
-          />
-          <div className="md:ml-5">
-            <h3 className="font-bold md:mb-2.5 md:text-xl lg:text-2xl xl:mb-5 xl:text-3xl">
+          <div
+            className="hidden shrink-0 overflow-hidden rounded-full md:block"
+          >
+            <img
+              src={speakerAvatar}
+              alt={`Headshot of ${speakerName}`}
+              className='h-40 w-40 object-cover'
+            />
+          </div>
+          <div
+            className="md:ml-5"
+          >
+            <h3
+              className="font-bold md:mb-2.5 md:text-xl lg:text-2xl xl:mb-5 xl:text-3xl"
+            >
               {sessionTitle}
             </h3>
             <p className="text-gray-700">by {speakerName}</p>
