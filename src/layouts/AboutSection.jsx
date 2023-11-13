65c553b391e6d0169704b74d1761a8e9386f57cc
Shashanka Prajapati <shashankap95@gmail.com>
2023-11-13 15:13:41 -0500

    [fix] Fix the import in about page

--- src/layouts/AboutSection.jsx
+++ src/layouts/AboutSection.jsx
@@ -1,32 +1,27 @@
-import aboutLogo from '@/assets/aboutlogo.png'
+import aboutLogo from '@/assets/aboutLogo.png'
 
 function AboutSection() {
   return (
-    <section id="about" className="relative h-screen flex flex-col items-center justify-center text-center">
+    <section
+      id="about"
+      className="relative flex h-screen flex-col items-center justify-center text-center"
+    >
+      <div id="text-aboutPage" className="mx-auto mb-8 max-w-screen-lg">
+        <p className="font-abc text-sm font-normal md:text-base lg:text-lg xl:text-xl ">
+          The Detroit and Ann Arbor Google Developer Groups (GDG) are teaming up
+          once again to bring you the 9th Michigan DevFest on December 2, 2023,
+          at the Little Caesar&apos;s Global. With our 9th DevFest, we aim to put
+          together a great day of networking and knowledge sharing for all
+          involved.
+        </p>
+      </div>
 
-    <div id="text-aboutPage" className='max-w-screen-lg mx-auto mb-8'>
-      <p className="font-abc font-normal text-sm md:text-base lg:text-lg xl:text-xl ">
-        The Detroit and Ann Arbor Google Developer Groups (GDG) are teaming up once again to bring you the 9th Michigan DevFest on December 2, 2023, at 
-        the Little Caesar's Global. With our 9th DevFest, we aim to put together a great day of networking and knowledge sharing for all involved.
-      </p>
-    </div>
-  
-    <img 
-      className="mx-auto object-cover w-full h-auto mt-10" 
-      src={aboutLogo}
-      alt="About Logo"
-    />
-  
-  </section>
-  
-
-  
-
-  
-
-
-  
-
+      <img
+        className="mx-auto mt-10 h-auto w-full object-cover"
+        src={aboutLogo}
+        alt="About Logo"
+      />
+    </section>
   )
 }
 
