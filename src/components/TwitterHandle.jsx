1bebbea914e0b85cd19349f11ec8fa688e7d90b8
Shashanka Prajapati <shashankap95@gmail.com>
2023-11-13 19:14:30 -0500

    [refactor] Fix formatting

--- src/components/TwitterHandle.jsx
+++ src/components/TwitterHandle.jsx
@@ -1,14 +1,18 @@
-import TwitterIcon from '@assets/TwitterIcon.svg';
+import TwitterIcon from '@/assets/TwitterIcon.svg'
 
 /* eslint-disable react/prop-types */
 
 function TwitterHandle(props) {
-
-    return (
-        <a href={props.url} target='_blank' rel='noreferrer' className='flex h-[60px] w-[60px] shrink-0 items-center justify-center rounded-full bg-white'>
-            <img className='h-[40px] w-[40px]' src={TwitterIcon} />
-        </a>
-    );
+  return (
+    <a
+      href={props.url}
+      target="_blank"
+      rel="noreferrer"
+      className="flex h-[60px] w-[60px] shrink-0 items-center justify-center rounded-full bg-white"
+    >
+      <img className="h-[40px] w-[40px]" src={TwitterIcon} />
+    </a>
+  )
 }
 
 export default TwitterHandle
