7aea46934b113b522f33dca66336b32904c46a9a
Shashanka Prajapati <shashankap95@gmail.com>
2023-11-13 19:01:18 -0500

    added better list prop names

---
+++ src/components/StylableComponentList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function StylableComponentList({ childComponentData, childComponent, className }) {
+  return (
+    <ul
+      className={className}
+    >
+      {childComponentData.map((item) => (
+        <li
+          key={item.id}
+        >
+          {React.createElement(childComponent, item)}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+StylableComponentList.propTypes = {
+  childComponentData: PropTypes.array.isRequired,
+  childComponent: PropTypes.object.isRequired,
+  className: PropTypes.string
+}
+
+export default StylableComponentList
