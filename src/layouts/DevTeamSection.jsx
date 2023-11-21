9abe19694ed3da4f815619cec083070fe7b2c6d1
rosy <rosyshrestha028@gmail.com>
2023-11-21 18:43:17 -0500

    Utilize the dev card component

--- src/layouts/DevTeamSection.jsx
+++ src/layouts/DevTeamSection.jsx
@@ -1,4 +1,4 @@
-import LinkedInHandle from '@/components/ui/LinkedInHandle'
+import DevTeamCard from '@/components/dev/DevTeamCard'
 import { devTeamData } from '@/data/dev'
 
 const DevTeamSection = () => {
@@ -9,28 +9,17 @@ const DevTeamSection = () => {
     >
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {devTeamData.map((dev, index) => (
-          <div
-            key={index}
-            className="group relative mb-10 ml-2 transform-gpu overflow-hidden grayscale transition-transform hover:scale-105 hover:cursor-pointer hover:filter-none"
-          >
-            <img
-              src={dev.avatar}
-              alt={`Image ${index + 1}`}
-              className="h-56 w-full object-cover object-center "
-            />
-            <LinkedInHandle handle={dev.linkedin} />
-            <div className="invisible h-24 translate-y-8 bg-primary-300 pt-5 text-center text-sm text-sky-900 transition ease-in-out group-hover:visible group-hover:translate-y-0">
-              <h1 className="text-base font-semibold text-sky-900">
-                {dev.name}
-              </h1>
-              <div className="px-2">
-                <h3 className="text-sm text-sky-900">
-                  {dev.organization || dev.university}
-                </h3>
-                <p className="text-sm text-sky-900">{dev.position}</p>
-              </div>
-            </div>
-          </div>
+          <DevTeamCard
+            key={`dev-${index}`}
+            id={index}
+            name={dev.name}
+            avatar={dev.avatar}
+            linkedin={dev.linkedin}
+            organization={dev.organization}
+            position={dev.position}
+            university={dev.university}
+            bio={dev.bio}
+          />
         ))}
       </div>
     </section>
