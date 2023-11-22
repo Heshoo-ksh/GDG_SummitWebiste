d6da19e2eab4e37f5c9d338717bbf206d59fb92a
Shashanka Prajapati <shashankap95@gmail.com>
2023-11-22 16:36:38 -0500

    [feat] Add responsive design to speaker details modal

--- src/components/ui/TwitterHandle.jsx
+++ src/components/ui/TwitterHandle.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types'
-
-import TwitterIcon from '@/assets/TwitterIcon.svg'
+import { FaXTwitter } from 'react-icons/fa6'
 
 const TwitterHandle = ({ handle }) => {
   return (
@@ -8,9 +7,9 @@ const TwitterHandle = ({ handle }) => {
       href={`https://twitter.com/${handle}`}
       target="_blank"
       rel="noreferrer"
-      className="absolute bottom-[-31px] left-1/2 flex h-[40px] w-[40px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-white"
+      className="absolute -bottom-8 left-1/2 flex h-10 w-10 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-white"
     >
-      <img className="relative h-6 w-6" src={TwitterIcon} alt="Twitter logo" />
+      <FaXTwitter className="h-6 w-6" />
     </a>
   )
 }
