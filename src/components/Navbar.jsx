003d0e7bca87a33bc52e95928caeb1bc373e33e3
Shashanka Prajapati <shashankap95@gmail.com>
2023-11-21 19:06:47 -0500

    [feat] Add responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { FaBars } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
   const [activeLink, setActiveLink] = useState('landing')
+  const [isNavVisible, setIsNavVisible] = useState(false)
+
+  const navRef = useRef(null)
 
   // Sections and corresponding links
   const sections = useMemo(
@@ -66,15 +70,45 @@ function Navbar() {
     }
   }
 
+  useEffect(() => {
+    // Function to close the nav when the user clicks outside of it
+    const handleClickOutside = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setIsNavVisible(false)
+      }
+    }
+
+    // Attach the click event listener
+    document.addEventListener('click', handleClickOutside)
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [])
+
   return (
-    <nav className="fixed left-0 top-0 z-10 w-full bg-white p-4 shadow">
-      <ul className="flex justify-end space-x-2 px-4 py-2">
+    <nav
+      ref={navRef}
+      className="fixed left-0 top-0 z-10 w-full bg-white p-4 shadow"
+    >
+      <button
+        className="rounded border-2 px-4 md:hidden"
+        onClick={() => setIsNavVisible(!isNavVisible)}
+      >
+        <FaBars className="h-10" />
+      </button>
+      <ul
+        className={`flex flex-col space-y-4 overflow-hidden md:flex-row md:justify-end md:space-x-2 md:space-y-0 md:px-4 md:py-2 ${
+          isNavVisible ? 'h-full' : 'h-0 md:h-full'
+        }`}
+      >
         {sections.map((section) => (
           <li key={section.id}>
             <Link
               to={`#${section.id}`}
               onClick={(event) => handleNavigation(event, section.id)}
-              className={`${section.id === 'landing' ? 'invisible' : ''} p-6 ${
+              className={`${section.id === 'landing' ? 'hidden' : ''} p-6 ${
                 activeLink === section.id ? 'text-primary-500' : ''
               }`}
             >
