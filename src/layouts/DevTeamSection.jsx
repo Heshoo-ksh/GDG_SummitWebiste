5772f9cb15819eb3a63faeb9e57d5fabdff39d50
rosy <rosyshrestha028@gmail.com>
2023-11-22 13:58:54 -0500

    Add header in dev team section

--- src/layouts/DevTeamSection.jsx
+++ src/layouts/DevTeamSection.jsx
@@ -5,8 +5,13 @@ const DevTeamSection = () => {
   return (
     <section
       id="devteam"
-      className="mb-20 flex flex-wrap items-center justify-center"
+      className="flex flex-wrap items-center justify-center bg-primary-100 pb-10"
     >
+      <div className="flex w-full sm:px-10 sm:pt-8 md:px-14 md:pt-12 lg:px-16 lg:pt-14">
+        <header className="mb-4 w-full text-center font-russell text-4xl md:mb-6 md:text-5xl lg:text-6xl">
+          Dev Team
+        </header>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {devTeamData.map((dev, index) => (
           <DevTeamCard
