8e6a29001ef5ae3ad950a513667c67e85b738707
Hisham Odeh <hesho@umich.edu>
2025-02-10 14:39:42 -0500

    Update landing page (#17)

--- src/layouts/LandingSection.jsx
+++ src/layouts/LandingSection.jsx
@@ -2,34 +2,43 @@
 import { FaTicketAlt } from 'react-icons/fa'
 
 import date from '@/assets/images/date.png'
-//import michiganDevFestLogoDark from '@/assets/images/devfest_logo_dark.png'
-import DevFestLogo from '@/assets/images/DevFestLogo.png'
+import BHMSummitLogo from '@/assets/images/BHMSummitLogo.png'
 
 function LandingSection() {
   return (
     <section id="landing" className="mt-16">
       <div className="flex flex-wrap bg-black">
-        <div className="w-full p-12 sm:w-1/2">
+        <div className="w-full sm:w-1/2">
           <img
             className="w-full"
-            src={DevFestLogo}
-            alt="Michigan DevFest Logo"
+            src={BHMSummitLogo}
+            alt="Black History Innovation Summit Logo"
           />
         </div>
 
         <div className="flex w-full flex-col justify-between bg-primary-400 sm:w-1/2">
           <div className="flex grow flex-col items-center justify-center space-y-8 p-10">
             <img className="w-3/4" src={date} alt="Date and Red Circle" />
-
+            <h1 className="text-justify text-xl font-semibold lg:text-base xl:text-3xl">
+              Discover the Future of Tech Innovation in Detroit
+            </h1>
             <p className="text-justify text-sm font-normal lg:text-base xl:text-lg">
-              GDG Detroit in partnership with Black Tech Saturdays, COMPASS, NSBE Detroit, Clixie AI, Think
-              Technologies, DTE Energy, and Grand Circus is hosting a day of
-              learning, gathering, and celebrating Detroit&apos;s tech
-              ecosystem. This large-scale, in-person summit at Michigan Central
-              Station will offer a full day on the latest advancements in tech
-              and transformative impact of AI on industries. Content will be
-              provided by local tech and business leaders, Google, Google
-              Developer Experts, including a Build with AI workshop.
+              On Saturday, February 15, 2025, Detroit will become the epicenter
+              of technology and innovation as local professionals, founders, and
+              early-career professionals gather for a day of learning, building,
+              and connecting.
+              <br />
+              <br />
+              In partnership with Google Developer Group Detroit, Google, Black
+              Tech Saturdays, Clixie AI, Compass, NSBE Detroit, DTE and Think
+              Technologies, this large-scale in-person event at Michigan Central
+              Station and New Lab will spotlight the latest advancements in
+              technology and the transformative impact of AI on industries.
+              <br />
+              <br />
+              Attendees will engage in hands-on workshops, product demos, and
+              panel discussions with industry leaders, culminating in a lively
+              social hour designed for networking and collaboration.
             </p>
             <a
               href="https://events.eventnoire.com/e/black-tech-saturdays-3?utm_source=affiliate&utm_name=GDG-Link&utm_campaign=GDG-Link&utm_id=679934b71770480eb6a500120a1e60ce"
