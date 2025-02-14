113daf71461f7d3d635f3aa4b624146f87cb1839
shams <sahson456@gmail.com>
2025-02-14 15:52:12 -0500

    works, need to fix spacing

--- src/data/organizers.js
+++ src/data/organizers.js
@@ -1,23 +1,39 @@
-import DaveKoziel from '@/assets/images/organizers/Dave Koziol.png'
-import JennaRitten from '@/assets/images/organizers/Jenna Ritten.png'
+import CCS from '@/assets/images/sponsors/CCS-logo.webp'
+import IBM from '@/assets/images/sponsors/IBM.webp'
+import Google from '@/assets/images/sponsors/Google.webp'
+import GrandCircus from '@/assets/images/sponsors/Grand Circus.webp'
+import DTE from '@/assets/images/sponsors/DTE.png'
+
 
 export const organizersData = [
   {
     id: 1,
-    name: 'Dave Koziel',
-    twitter: 'davekoziol',
-    avatar: DaveKoziel,
-    organization: 'Little Caesars',
-    position: 'GDG Organizer',
-    url: 'https://www.linkedin.com/in/davekoziol/',
+    name: 'CCS',
+    avatar: CCS,
+    url: 'https://bit.ly/ccs-jobs-25',
   },
   {
     id: 2,
-    name: 'Jenna Ritten',
-    twitter: 'jritten',
-    avatar: JennaRitten,
-    organization: 'IBM',
-    position: 'GDG Organizer',
-    url: 'https://gdg.community.dev/u/mbhdq6/',
+    name: 'DTE',
+    avatar: DTE,
+    url: 'https://bit.ly/dte-jobs-25',
+  },
+  {
+    id: 3,
+    name: 'Google',
+    avatar: Google,
+    url: 'https://bit.ly/google-jobs-25',
+  },
+  {
+    id: 4,
+    name: 'Grand Circus',
+    avatar: GrandCircus,
+    url: 'https://bit.ly/grandcircus-jobs-25',
+  },
+  {
+    id: 5,
+    name: 'IBM',
+    avatar: IBM,
+    url: 'https://bit.ly/ibm-jobs-25',
   },
 ]
--- src/layouts/OrganizersSection.jsx
+++ src/layouts/OrganizersSection.jsx
@@ -10,15 +10,15 @@ function OrganizersSection() {
     >
       <div className="flex w-full justify-between px-8 pt-6 sm:px-10 sm:pt-8 md:px-14 md:pt-12 lg:px-16 lg:pt-14">
         <header className="w-full text-center font-russell text-4xl md:text-5xl lg:text-6xl">
-          Organizers
+          Job Boards
         </header>
         <img
           src={Arrow}
-          alt="Organizers"
+          alt="Job Boards"
           className="h-8 -scale-x-100 md:h-10 lg:h-12"
         />
       </div>
-      <div className="mt-32 grid grid-cols-1 gap-20 sm:grid-cols-2 lg:mt-36">
+      <div className="mt-32 grid grid-cols-1 gap-20 sm:grid-cols-3 lg:mt-36">
         {organizersData.map((organizer) => (
           <GenericCard
             key={organizer.id}
