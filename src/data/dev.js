6b74974b35b2c24faf44c7005043a04e8b23dab6
Rehanashri <rehgr@umich.edu>
2025-02-14 17:57:02 -0500

    Updated all speakers and respective session details for Founders & Funders and Detroit Ecosystem tracks

--- src/data/dev.js
+++ src/data/dev.js
@@ -1,4 +1,5 @@
 import Hisham_Odeh from '@/assets/images/dev/Hisham_Odeh.jpg'
+import Rehanashri from '@/assets/images/dev/Rehanashri.jpg'
 
 export const devTeamData = [
   {
@@ -12,4 +13,16 @@ export const devTeamData = [
     position: 'Junior Software Engineer',
     avatar: Hisham_Odeh,
   },
+  {
+    id: 2,
+    name: 'Rehanashri Rastapuramata',
+    bio: '',
+    linkedin: 'https://www.linkedin.com/in/rehanashri/',
+    github: 'https://github.com/Rehanashri',
+    university: 'University Of Michigan - Dearborn',
+    organization: '',
+    position: 'Graduate Student',
+    avatar: Rehanashri,
+  },
+
 ]
