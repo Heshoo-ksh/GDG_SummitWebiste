635805f33536a8c04d2beb6683a803350d2d2cff
Shams Ahson <101009062+shams-ahson@users.noreply.github.com>
2025-02-15 03:55:58 -0500

    Shams (#49)
    * updating found-fund
    
    * shams
    
    * update
    
    ---------
    
    Co-authored-by: Heshoo-ksh <hishamodeh2001@gmail.com>

--- src/data/dev.js
+++ src/data/dev.js
@@ -2,6 +2,7 @@ import Hisham_Odeh from '@/assets/images/dev/Hisham_Odeh.jpg'
 import Rehanashri from '@/assets/images/dev/Rehanashri.jpg'
 import JesusOrozco from '@/assets/images/dev/Jesus Orozco.jpg'
 import AditiDharmadhikari from '@assets/images/dev/Aditi_Dharmadhikari.jpg'
+import Shams from '@assets/images/dev/shams.jpg'
 
 export const devTeamData = [
   {
@@ -39,7 +40,7 @@ export const devTeamData = [
     avatar: JesusOrozco,
   },
   {
-  id: 4,
+    id: 4,
     name: 'Aditi Dharmadhikari',
     bio: '',
     linkedin: 'https://www.linkedin.com/in/aditi-d4/',
@@ -48,5 +49,16 @@ export const devTeamData = [
     organization: '',
     position: 'Sophomore',
     avatar: AditiDharmadhikari,
-  }
+  },
+  {
+    id: 5,
+    name: 'Shams Ahson',
+    bio: '',
+    linkedin: 'https://www.linkedin.com/in/shams-ahson/',
+    github: 'https://github.com/shams-ahson',
+    university: 'University Of Michigan - Dearborn',
+    organization: 'Google Developer Groups Lead',
+    position: 'Senior',
+    avatar: Shams,
+  },
 ]
